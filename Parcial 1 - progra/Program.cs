f892eb4df7965b3ca1c03181bbed4a9752a3799c
Alumnos <Alumnos@LABC2PC19>
2022-09-02 10:42:44 -0600

    Agregar archivos de proyecto.

---
+++ Parcial 1 - progra/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Parcial_1___progra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
